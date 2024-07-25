db36148da2ec5b17b75dcd9f3d91f74639b322d7
LuisFelipeRiesPereira <luisfeliperuespereira@gmail.com>
2024-07-25 00:08:51 -0300

    testando 2

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/react-study/"
+  base: "/react-study/",
+  plugins: [react()]
 })
 
 
